b11ba978f19c099022ea875d8c96fddd5c63f570
Ningthoujam Borison Singh <ningthoujamborison33@gmail.com>
2022-08-26 17:17:32 +0530

    gg

--- app/src/main/java/mn/athen/test/Otpverify.kt
+++ app/src/main/java/mn/athen/test/Otpverify.kt
@@ -1,11 +1,18 @@
 package mn.athen.test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Otpverify : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otpverify)
+    }
+
+    fun confirm(view: View) {
+        startActivity(Intent(this,Landing::class.java))
+        finish()
     }
 }
