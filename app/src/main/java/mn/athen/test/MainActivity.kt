d05450df785ca7876e69b7bf2765890e10c0d679
Ningthoujam Borison Singh <ningthoujamborison33@gmail.com>
2022-08-26 17:07:38 +0530

    otp

--- app/src/main/java/mn/athen/test/MainActivity.kt
+++ app/src/main/java/mn/athen/test/MainActivity.kt
@@ -1,11 +1,19 @@
 package mn.athen.test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendotp(view: View) {
+        Toast.makeText(this,"Otp Send",Toast.LENGTH_LONG).show()
+        startActivity(Intent(this,Otpverify::class.java))
     }
 }
