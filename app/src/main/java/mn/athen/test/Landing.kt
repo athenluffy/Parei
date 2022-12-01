d3309e6fcf1b5cf8dd76ee316611abe303665465
Ningthoujam Borison Singh <ningthoujamborison33@gmail.com>
2022-12-01 15:05:23 +0530

    Bottom Menu

--- app/src/main/java/mn/athen/test/Landing.kt
+++ app/src/main/java/mn/athen/test/Landing.kt
@@ -2,10 +2,39 @@ package mn.athen.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class Landing : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
+        var btmNavigation = R.id.landingBtmNavigation
+
+       NavigationBarView.OnItemSelectedListener{item->
+           when(item.itemId)
+           {
+               R.id.home -> {
+                   Toast.makeText(this,"Home",Toast.LENGTH_SHORT).show()
+                   // Respond to navigation item 1 click
+                   true
+               }
+               R.id.cart -> {
+                   Toast.makeText(this,"Cart",Toast.LENGTH_SHORT).show()
+                   // Respond to navigation item 1 click
+                   true
+               }
+               R.id.accoount -> {
+                   Toast.makeText(this,"Profile",Toast.LENGTH_SHORT).show()
+                   // Respond to navigation item 1 click
+                   true
+               }
+               else -> false
+           }
+       }
+
+
     }
 }
