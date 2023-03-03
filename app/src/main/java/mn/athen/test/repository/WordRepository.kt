3a0b928cfe34eccbbfe83b2ea7ef410588ea86a8
Ningthoujam Borison Singh <ningthoujamborison33@gmail.com>
2023-03-03 17:31:19 +0530

    Dependency Injection

--- app/src/main/java/mn/athen/test/repository/WordRepository.kt
+++ app/src/main/java/mn/athen/test/repository/WordRepository.kt
@@ -1,36 +1,28 @@
 package mn.athen.test.repository
 
-import android.app.Application
 import androidx.lifecycle.LiveData
 import mn.athen.test.Class.Word
-import mn.athen.test.dao.WordDao
-import mn.athen.test.db.WordDatabase.Companion.invoke
+import mn.athen.test.db.WordDatabase
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-class WordRepository(application: Application?)  {
-    val allWords: LiveData<List<Word>>
-
-    init {
-        val db = invoke(application!!)
-        wordDao = db.wordDao()
-        allWords = wordDao.allWords
-    }
+class WordRepository(private val db:WordDatabase)  {
+    val allWords: LiveData<List<Word>> = db.wordDao().allWords
 
     fun insert(word: Word?) {
         //insert to Database needs to be called async
 
-        service.execute { wordDao.insert(word) }
+        service.execute { db.wordDao().insert(word) }
     }
 
     fun delete(word: Word?) {
         //delete to Database needs to be called async
 
-        service.execute { wordDao.deleteword(word) }
+        service.execute { db.wordDao().deleteword(word) }
     }
 
     companion object {
-        private lateinit var wordDao: WordDao
+
         val service: ExecutorService = Executors.newSingleThreadExecutor()
     }
 }
