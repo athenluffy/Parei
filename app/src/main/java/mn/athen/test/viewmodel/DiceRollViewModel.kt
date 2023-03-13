80a33f554af8a451a68f351fba3529d0c35651ab
Ningthoujam Borison Singh <ningthoujamborison33@gmail.com>
2023-03-13 15:45:14 +0530

    Mutable State Flow

--- app/src/main/java/mn/athen/test/viewmodel/DiceRollViewModel.kt
+++ app/src/main/java/mn/athen/test/viewmodel/DiceRollViewModel.kt
@@ -1,14 +1,31 @@
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlin.random.Random
 
-
+data class DiceUiState(
+    val firstDieValue: Int? = null,
+    val secondDieValue: Int? = null,
+    val numberOfRolls: Int = 0,
+)
 //Implement View model
 class DiceRollViewModel : ViewModel()
 {
-    val scoreTeamA : Int? = 0
-    val scoreTeamB :Int? =0
+    private val _DiceUiState = MutableStateFlow( DiceUiState())
 
-    fun getLoggedInUser()
-    {
+    val uiState : StateFlow<DiceUiState> = _DiceUiState.asStateFlow()
 
-    }
+    fun rollDice() {
+        _DiceUiState.update {
+            it.copy(
+                firstDieValue = Random.nextInt(1,7),
+                secondDieValue = Random.nextInt(1,7),
+                numberOfRolls=it.numberOfRolls+1
+
+            )
+        }
+        }
+
 }
